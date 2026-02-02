e28531a79b7fd4b1a21a7045e4b80f5507a9549a
Hz <67035001@kmitl.ac.th>
2026-02-03 00:29:07 +0700

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web/', // Replace 'web' with your GitHub repository name
+  base: '/', // Replace 'web' with your GitHub repository name
 })
